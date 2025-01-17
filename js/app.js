60fc42d44351369af9c24c3ae3bf203270570ad9
miguelmaar842 <miguelangelmaar842@gmail.com>
2025-01-16 23:29:28 -0600

    PortafolioV1

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// const toggleBtn = document.querySelector('.toggle_btn')
+// const toggleBtnIcon = document.querySelector('.toggle_btn i')
+// const dropDownMenu = document.querySelector('.dropdown_menu')
+
+// toggleBtn.onclick = function(){
+//     dropDownMenu.classList.toggle('open')
+//     const isOpen =dropDownMenu.classList.contains('open')
+
+//     ? 'fa-solid fa-xmark'
+//     : 'fa-solid fa-bars'
+// }
+// let menuList = document.getElementById("menuList")
+// menuList.style.maxHeight = "0px";
+
+// function toggleMenu() {
+//     if (menuList.style.maxHeight == "0px") {
+//         menuList.style.maxHeight = "300px";
+//     }
+//     else {
+//         menuList.style.maxHeight = "0px";
+//     }
+// }
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+//         const target = document.querySelector(this.getAttribute('href'));
+//         window.scrollTo({
+//             top: target.offsetTop - document.querySelector('nav').offsetHeight, // Restamos la altura del nav
+//             behavior: 'smooth' // Desplazamiento suave
+//         });
+//     });
+// });
+
